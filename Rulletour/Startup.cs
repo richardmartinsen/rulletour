33dfd6831ce4b50a3187e32d62b5b9ab7b295b18
Richard Martinsen <github@martinsen.org>
2014-12-13 13:49:42 +0100

    Start

---
+++ Rulletour/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rulletour.Startup))]
+namespace Rulletour
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
